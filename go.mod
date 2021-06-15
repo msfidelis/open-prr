9095ffcf014c1524800c4927c4c9e44ec2089b9c
Matheus Scarpato Fidelis <msfidelis01@gmail.com>
2021-06-15 17:14:37 -0300

    REST Interface

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module open-prr
 go 1.15
 
 require (
+	github.com/gin-contrib/logger v0.0.3
+	github.com/gin-gonic/gin v1.7.2
 	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/lib/pq v1.10.2
+	github.com/msfidelis/gin-chaos-monkey v0.0.5
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/rs/zerolog v1.18.0
 )
--- main.go
+++ main.go
@@ -1,9 +1,79 @@
 package main
 
 import (
+	"github.com/gin-contrib/logger"
+	"open-prr/controllers/healthcheck"
+	"open-prr/controllers/liveness"
+	"open-prr/controllers/readiness"
+
+	"open-prr/pkg/memory_cache"
 	"open-prr/pkg/migration"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/gin-gonic/gin"
+
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if gin.IsDebugging() {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	// logger
+	log.Logger = log.Output(
+		zerolog.ConsoleWriter{
+			Out:     os.Stderr,
+			NoColor: false,
+		},
+	)
+	subLog := zerolog.New(os.Stdout).With().Logger()
+
+	// Memory Cache Singleton
+	c := memory_cache.GetInstance()
+
+	// Readiness Probe Mock Config - Warmup in Seconds
+	probe_time_raw := os.Getenv("READINESS_PROBE_MOCK_TIME_IN_SECONDS")
+	if probe_time_raw == "" {
+		probe_time_raw = "5" // 5 Seconds after boot to success readiness response ok
+	}
+	probe_time, err := strconv.ParseUint(probe_time_raw, 10, 64)
+	if err != nil {
+		fmt.Println("Environment variable READINESS_PROBE_MOCK_TIME_IN_SECONDS conversion error", err)
+	}
+
+	// Set time in Memory Cache
+	c.Set("readiness.ok", "false", time.Duration(probe_time)*time.Second)
+
+	// Migration
 	migration.Migrate()
+
+	// New Router
+	router := gin.New()
+
+	//Middlewares
+	router.Use(gin.Recovery())
+
+	router.Use(logger.SetLogger(logger.Config{
+		Logger:   &subLog,
+		UTC:      true,
+		SkipPath: []string{"/skip"},
+	}))
+
+	// Healthcheck Router
+	router.GET("/healthcheck", healthcheck.Ok)
+
+	// Liveness and Readiness
+	router.GET("/liveness", liveness.Ok)
+	router.GET("/readiness", readiness.Ok)
+
+	router.Run()
+
 }
